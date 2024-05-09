0493aa4c0b72a1d297b4d626e147d5d435d75a7e
mckayfawcett <mckay.fawcett@gmail.com>
2024-05-09 13:34:12 -0600

    added lan survey folder, and survey app with django

---
+++ lansurvery/lansurvery/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lansurvery project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lansurvery.settings')
+
+application = get_wsgi_application()
